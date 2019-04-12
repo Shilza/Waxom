f27a99fdb655d487ff309b6490c1f7802388472d
Shilza <andgalatinov@gmail.com>
2019-04-12 20:40:12 +0300

    Updates

---
+++ precache-manifest.7253956ab8b354a3b8d90a34f2dc4f9f.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "d029454e9889ea79e9c9697f9ea6f041",
+    "url": "/Waxom/static/media/project-4.d029454e.jpg"
+  },
+  {
+    "revision": "835d4ee07f1da91ef2f3",
+    "url": "/Waxom/static/css/main.0063bb45.chunk.css"
+  },
+  {
+    "revision": "cd02093709f7cfe43d8a",
+    "url": "/Waxom/static/js/runtime~main.6b974fef.js"
+  },
+  {
+    "revision": "cc4bb18faa2f41019b4c",
+    "url": "/Waxom/static/js/2.6d6dce9b.chunk.js"
+  },
+  {
+    "revision": "b519cb8c4f4f398dfbf7ae5e277ec3f0",
+    "url": "/Waxom/static/media/mainBackground.b519cb8c.jpg"
+  },
+  {
+    "revision": "f9333ee305cf357a71b67facd0880d3b",
+    "url": "/Waxom/static/media/logosBackground.f9333ee3.jpg"
+  },
+  {
+    "revision": "9200e0a5ed2aa3a3894cf287f2a49745",
+    "url": "/Waxom/static/media/search.9200e0a5.svg"
+  },
+  {
+    "revision": "02fe75dbb6076cfdb902fe45215a43b0",
+    "url": "/Waxom/static/media/cart.02fe75db.svg"
+  },
+  {
+    "revision": "c5ebb6bd05ab5de7ba74ca7f9ff3fdfa",
+    "url": "/Waxom/static/media/left-arrow.c5ebb6bd.svg"
+  },
+  {
+    "revision": "5965eb7ba79f9af22601c3d968d11c82",
+    "url": "/Waxom/static/media/right-arrow.5965eb7b.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/Waxom/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "46851702dfd5d0d47e96afb3b2f54ee4",
+    "url": "/Waxom/static/media/layers.46851702.svg"
+  },
+  {
+    "revision": "533e1d8eaac7b27d4e5dd5360ae203b7",
+    "url": "/Waxom/static/media/gear.533e1d8e.svg"
+  },
+  {
+    "revision": "6c01830a328fcbefe21018f962379798",
+    "url": "/Waxom/static/media/money-bag.6c01830a.svg"
+  },
+  {
+    "revision": "903904e08f79d7efa923ff32925d961e",
+    "url": "/Waxom/static/media/ideasBackground.903904e0.jpg"
+  },
+  {
+    "revision": "94db89aed96524115a96568c89bf1fd9",
+    "url": "/Waxom/static/media/link.94db89ae.svg"
+  },
+  {
+    "revision": "619a7eeb47f1d8a00e295fc60223f8f6",
+    "url": "/Waxom/static/media/search.619a7eeb.svg"
+  },
+  {
+    "revision": "66c73023c45f93ccf8909a852f61345e",
+    "url": "/Waxom/static/media/project-1.66c73023.jpg"
+  },
+  {
+    "revision": "5debd4f4ce27567ff43e41d6b6aeee44",
+    "url": "/Waxom/static/media/project-2.5debd4f4.jpg"
+  },
+  {
+    "revision": "51281f55f456dadc6407971e3cc81ce5",
+    "url": "/Waxom/static/media/project-3.51281f55.jpg"
+  },
+  {
+    "revision": "835d4ee07f1da91ef2f3",
+    "url": "/Waxom/static/js/main.67165560.chunk.js"
+  },
+  {
+    "revision": "0d8b238cfd5297d3c6d036c6610c068c",
+    "url": "/Waxom/static/media/project-5.0d8b238c.jpg"
+  },
+  {
+    "revision": "15edd65945f0d64e07c96f902b2d30fe",
+    "url": "/Waxom/static/media/project-6.15edd659.jpg"
+  },
+  {
+    "revision": "2d6bb830cc860d1d020190ae1e7e31d4",
+    "url": "/Waxom/static/media/videoBackground.2d6bb830.jpg"
+  },
+  {
+    "revision": "c6525ff798bf217595c2bd0e1619f373",
+    "url": "/Waxom/static/media/play-button.c6525ff7.svg"
+  },
+  {
+    "revision": "76b77bb4259f56b910cc1e6cda964377",
+    "url": "/Waxom/static/media/prone.76b77bb4.jpg"
+  },
+  {
+    "revision": "4f313d4935e64a3d1b38978f5b7573f3",
+    "url": "/Waxom/static/media/users.4f313d49.svg"
+  },
+  {
+    "revision": "d3f4d1c34fee35f25258107cc79f2b20",
+    "url": "/Waxom/static/media/coffee.d3f4d1c3.svg"
+  },
+  {
+    "revision": "75def000174d8a1826c036f2d9211fa8",
+    "url": "/Waxom/static/media/post.75def000.svg"
+  },
+  {
+    "revision": "8d36686d677925ac3b5f26b6f7502aba",
+    "url": "/Waxom/static/media/like.8d36686d.svg"
+  },
+  {
+    "revision": "cc297680e1f4c47404d6604d0bb2495e",
+    "url": "/Waxom/static/media/rocket.cc297680.svg"
+  },
+  {
+    "revision": "2af8d3c8d46c86a07583f64f943cc3b3",
+    "url": "/Waxom/static/media/post-1.2af8d3c8.jpg"
+  },
+  {
+    "revision": "60b7d4c7e4285d11c809e61cab507066",
+    "url": "/Waxom/static/media/post-2.60b7d4c7.jpg"
+  },
+  {
+    "revision": "932a3ddf71707203343b591800317af6",
+    "url": "/Waxom/static/media/post-3.932a3ddf.jpg"
+  },
+  {
+    "revision": "6b95da1c79f324880e7960aef85064f4",
+    "url": "/Waxom/static/media/logo-01.6b95da1c.png"
+  },
+  {
+    "revision": "86aa48db9e76d2cf63798037612e0f0a",
+    "url": "/Waxom/static/media/logo-02.86aa48db.png"
+  },
+  {
+    "revision": "85366a3076a5cd65f45dee58be60b23a",
+    "url": "/Waxom/static/media/logo-04.85366a30.png"
+  },
+  {
+    "revision": "1d8d999fda5641366ea268b396d612f2",
+    "url": "/Waxom/static/media/widget-1.1d8d999f.jpg"
+  },
+  {
+    "revision": "44718da1b1a2d4666dfbd0d243730b0c",
+    "url": "/Waxom/static/media/widget-4.44718da1.jpg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/Waxom/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "36d91c229cc8099aa118ec6e4bcd29d6",
+    "url": "/Waxom/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Waxom/precache-manifest.7253956ab8b354a3b8d90a34f2dc4f9f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Waxom/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
